6666fe9de932a0004d31f9b0562952d4ce4bf435
jimmy <1396974830@qq.com>
2018-07-09 17:55:28 +0800

    移植完成

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,8 @@
 #include <jni.h>
 #include <string>
 #include <thread>
-#include <Livesssmediaserver.h>
+#include <android/log.h>
+#include "Livesssmediaserver.h"
 
 void start_server1(){
     start_server();
@@ -16,6 +17,8 @@ Java_com_vf_cantimage_MainActivity_stringFromJNI(
     std::thread *thread = new std::thread(start_server1);
     thread->detach();
 
+//    start_server1();
+    __android_log_print(ANDROID_LOG_ERROR, "VFConfig", "Native registration unable to find class '%s'\n", "fuck");
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/vf/cantimage/MainActivity.kt
+++ app/src/main/java/com/vf/cantimage/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.vf.cantimage
 
+import android.Manifest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.concurrent.thread
+import android.support.v4.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.Manifest.permission
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var hell = thread { stringFromJNI() }
-        hell.start()
+        myPermission()
+        stringFromJNI()
 
         // Example of a call to a native method
     }
@@ -28,6 +36,20 @@ class MainActivity : AppCompatActivity() {
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
+        }
+    }
+
+    private val REQUEST_EXTERNAL_STORAGE = 1
+    private val PERMISSIONS_STORAGE = arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun myPermission() {
+        val permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            )
         }
     }
 }
