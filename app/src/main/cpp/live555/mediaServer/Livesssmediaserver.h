3d645db765292c1f84ce999cc453fcca96741c2c
jimmy <1396974830@qq.com>
2018-07-09 18:27:03 +0800

    hell

---
+++ app/src/main/cpp/live555/mediaServer/Livesssmediaserver.h
@@ -0,0 +1,9 @@
+//
+// Created by Administrator on 2018/7/9.
+//
+
+#ifndef CANTIMAGE_LIVESSSMEDIASERVER_H
+#define CANTIMAGE_LIVESSSMEDIASERVER_H
+
+int start_server(int argc = 0, char** argv = nullptr);
+#endif //CANTIMAGE_LIVESSSMEDIASERVER_H
